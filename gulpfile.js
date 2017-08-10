603ff095210de6e6682be12f96ba6c3ec14e1757
huanran <huanranjoe@163.com>
2017-08-10 03:25:21 -0700

    stylelint

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var htmlhint = require("gulp-htmlhint");
+var gulpStylelint = require('gulp-stylelint');
 
 gulp.task('project',function(){
 	return gulp.src('project/sass/style.scss')
@@ -7,3 +9,21 @@ gulp.task('project',function(){
 		.pipe(sass())
 		.pipe(gulp.dest('project/css'))
 	});
+
+gulp.task('html',function(){
+	return gulp.src("./project/*.html")
+		.pipe(htmlhint())
+    .pipe(htmlhint.reporter())
+    .pipe(htmlhint.failReporter())
+})
+
+gulp.task('lint-css', function lintCssTask() {
+
+  return gulp
+    .src('./project/css/style.css')
+    .pipe(gulpStylelint({
+      reporters: [
+        {formatter: 'string', console: true}
+      ]
+    }));
+});
